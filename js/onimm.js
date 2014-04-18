609dc48edda57a005638c230c2fb5ca67fce48cb
Elfhir <ta.thanhliem@gmail.com>
2014-04-18 11:50:39 +0200

    Fix Zoom behavior translation, but still flicker a bit

--- js/onimm.js
+++ js/onimm.js
@@ -30,7 +30,8 @@ function Onimm(id, data_uri) {
 		background_color : "#eee",
 		stroke_color : "#111",
 		totalNodes : 0,
-		zoom : null
+		zoom : null,
+		countZoom : 0
 	};
 
 	/**
@@ -38,9 +39,12 @@ function Onimm(id, data_uri) {
 	 */
 	onimm.init = function() {
 
+		// -------- When zoom and panning -----
 		onimm.settings.zoom = d3.behavior.zoom()
 			.scaleExtent([1, 10])
-			.on("zoom", onimm.zoomed);
+			.on("zoomstart", onimm.zoomstart)
+			.on("zoom", onimm.zoomed)
+			.on("zoomend", onimm.zoomend);
 
 		// -------------- When dragging -----
 		onimm.settings.drag = d3.behavior.drag()
@@ -52,11 +56,13 @@ function Onimm(id, data_uri) {
 		onimm.svg = d3.select(onimm.settings.id).append("svg:svg")
 			.attr("width", onimm.settings.width)
 			.attr("height", onimm.settings.height)
+			.attr("align", "center")
 			.style("border", "1px solid black")
-			.attr("id", id + "svg_")
-		.append("g")
-			.attr("transform", "translate(" + onimm.settings.margin.left + "," + onimm.settings.margin.right + ")")
-			.call(onimm.settings.zoom);
+			.attr("id", id + "svg_");
+
+		onimm.g = onimm.svg.append("g")
+			//.attr("transform", "translate(" + onimm.settings.margin.left + "," + onimm.settings.margin.right + ")")
+			.attr("id", "g_");
 
 		onimm.rect = onimm.svg.append("svg:rect")
 			.attr("width", onimm.settings.width)
@@ -65,6 +71,8 @@ function Onimm(id, data_uri) {
 			.style("pointer-events", "all");
 
 		onimm.container = onimm.svg.append("g")
+			.attr("transform", "translate(" + 0.5*onimm.settings.width + "," + 0.5*onimm.settings.height + ")")
+			.attr("class", "g_container_");
 
 		onimm.container.append("g")
 			.attr("class", "x axis")
@@ -110,23 +118,31 @@ function Onimm(id, data_uri) {
 			onimm.jobs.append("svg:circle")
 					.attr("class", "circle")
 					.attr("r", 20)
-					.attr("cx", function(d,i) {return onimm.x_coordinates(i);})
-					.attr("cy", function(d,i) {return onimm.y_coordinates(i);});
+					.attr("cx", function(d,i) {return d.x = onimm.x_coordinates(i);})
+					.attr("cy", function(d,i) {return d.y = onimm.y_coordinates(i);});
 			onimm.jobs.append("svg:text")
 					.attr("class", "data-text")
-					.attr("x", function(d,i) {return onimm.x_coordinates(i);})
-					.attr("y", function(d,i) {return onimm.y_coordinates(i);})
+					.attr("x", function(d,i) {return d.x = onimm.x_coordinates(i);})
+					.attr("y", function(d,i) {return d.y = onimm.y_coordinates(i);})
 					.text(function(d,i) {return d.name;});
 			
 			onimm.jobs.call(onimm.settings.drag);
-		
-				
+			onimm.svg.call(onimm.settings.zoom);
 
 		}); // End d3.json(uri,function)
 	};
 
-	onimm.zoomed = function() {
-		onimm.container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+	onimm.zoomstart = function(d) {
+		d3.event.sourceEvent.stopPropagation();
+	};
+
+	onimm.zoomed = function(d) {
+		console.log("X : " + d3.event.translate[0] + "  Y : " + d3.event.translate[1]);
+		onimm.svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+	};
+
+	onimm.zoomend = function(d) {
+
 	};
 
 	onimm.dragstarted = function(d) {
@@ -136,25 +152,30 @@ function Onimm(id, data_uri) {
 
 	// Admitted the dragged element is a svg group g with internal circle and text
 	onimm.dragged = function(d) {
-		d3.select(this).select('circle').attr("cx", d.x = d3.event.x + 0.5*onimm.settings.width).attr("cy", d.y = d3.event.y + 0.5*onimm.settings.height);
-		d3.select(this).attr("transfom", "translate("+ d3.event.x + 0.5*onimm.settings.width +","+ d3.event.y + 0.5*onimm.settings.height+")");
-		d3.select(this).select('text').attr("x", d.x = d3.event.x + 0.5*onimm.settings.width).attr("y", d.y = d3.event.y + 0.5*onimm.settings.height);
+		d3.select(this).select('circle').attr("cx", d3.event.x ).attr("cy", d3.event.y);
+		d3.select(this).attr("transfom", "translate("+ d3.event.x +","+ d3.event.y +")");
+		d3.select(this).select('text').attr("x", d3.event.x).attr("y", d3.event.y);
+		d.x = d3.event.x;
+		d.y = d3.event.y;
 	};
 
 	onimm.dragended = function(d) {
 		d3.select(this).classed("dragging", false);
 	};
 
+	/**
+	 * Initiate the jobs position with coordinates from polar
+	 * @param  {integer} i zero-based index of element
+	 * @return {float} x coordinates
+	 */
 	onimm.x_coordinates = function(i) {
-		var x_coordinates = 0.5*(onimm.settings.height*Math.cos(i*(2*Math.PI)/onimm.settings.totalNodes));
-		console.log("x"+i+": "+ x_coordinates);
-		return x_coordinates + 0.5*onimm.settings.width;
+		var x_coordinates = 0.4*(onimm.settings.height*Math.cos(i*(2*Math.PI)/onimm.settings.totalNodes));
+		return x_coordinates;
 	}
 
 	onimm.y_coordinates = function(i) {
-		var y_coordinates = 0.5*(onimm.settings.height*Math.sin(i*(2*Math.PI)/onimm.settings.totalNodes));
-		console.log("y"+i+": "+ y_coordinates);
-		return y_coordinates + 0.5*onimm.settings.height;
+		var y_coordinates = 0.4*(onimm.settings.height*Math.sin(i*(2*Math.PI)/onimm.settings.totalNodes));
+		return y_coordinates;
 	}
 
 	// Let it go, let it go !
@@ -167,5 +188,5 @@ function Onimm(id, data_uri) {
 onimm = Onimm("#onimm_", "http://www.jeremy-ta.fr/work/onisep/d3/data/test2.json");
 
 // DEBUG
-console.dir(onimm);
+//console.dir(onimm);
 
