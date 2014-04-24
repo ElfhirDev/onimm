4a9615b841db820aa1750f08d8ad5e38e7b2c11f
Elfhir <ta.thanhliem@gmail.com>
2014-04-24 18:33:54 +0200

    Fixed problem with stuttering elements, remove zoom behavior set bubble image

--- js/onimm.js
+++ js/onimm.js
@@ -21,17 +21,18 @@ function Onimm(id, data_uri) {
 	var onimm = {};
 
 	onimm.settings = {
-		id : id,
+		id : "#"+id,
 		data_uri : data_uri,
 		data : [],
 		margin : {top: -5, right: -5, bottom: -5, left: -5},
-		width : 640,
+		width : 600,
 		height : 400,
+		radius:20,
 		background_color : "#eee",
 		stroke_color : "#111",
 		totalNodes : 0,
 		zoom : null,
-		countZoom : 0
+		current : {x : 0, y : 0}
 	};
 
 	/**
@@ -39,20 +40,20 @@ function Onimm(id, data_uri) {
 	 */
 	onimm.init = function() {
 
-		// -------- When zoom and panning -----
+		// ---- When panning the background -----
 		onimm.settings.zoom = d3.behavior.zoom()
-			.scaleExtent([1, 10])
+			.scaleExtent([1, 1])
 			.on("zoomstart", onimm.zoomstart)
 			.on("zoom", onimm.zoomed)
 			.on("zoomend", onimm.zoomend);
 
 		// -------------- When dragging -----
 		onimm.settings.drag = d3.behavior.drag()
-			.origin(function(d) { return d; })
 			.on("dragstart", onimm.dragstarted)
 			.on("drag", onimm.dragged)
 			.on("dragend", onimm.dragended);
 
+		// ---------- Create SVG element -----
 		onimm.svg = d3.select(onimm.settings.id).append("svg:svg")
 			.attr("width", onimm.settings.width)
 			.attr("height", onimm.settings.height)
@@ -60,39 +61,10 @@ function Onimm(id, data_uri) {
 			.style("border", "1px solid black")
 			.attr("id", id + "svg_");
 
-		onimm.g = onimm.svg.append("g")
-			//.attr("transform", "translate(" + onimm.settings.margin.left + "," + onimm.settings.margin.right + ")")
-			.attr("id", "g_");
-
-		onimm.rect = onimm.svg.append("svg:rect")
-			.attr("width", onimm.settings.width)
-			.attr("height", onimm.settings.height)
-			.style("fill", "none")
-			.style("pointer-events", "all");
-
+		// -- Create container of elements -----
 		onimm.container = onimm.svg.append("g")
 			.attr("transform", "translate(" + 0.5*onimm.settings.width + "," + 0.5*onimm.settings.height + ")")
 			.attr("class", "g_container_");
-
-		onimm.container.append("g")
-			.attr("class", "x axis")
-		  .selectAll("line")
-			.data(d3.range(0, onimm.settings.width, 10))
-		  .enter().append("line")
-			.attr("x1", function(d) { return d; })
-			.attr("y1", 0)
-			.attr("x2", function(d) { return d; })
-			.attr("y2", onimm.settings.height);
-
-		onimm.container.append("g")
-			.attr("class", "y axis")
-		  .selectAll("line")
-			.data(d3.range(0, onimm.settings.height, 10))
-		  .enter().append("line")
-			.attr("x1", 0)
-			.attr("y1", function(d) { return d; })
-			.attr("x2", onimm.settings.width)
-			.attr("y2", function(d) { return d; });
 
 
 		// Load our resources
@@ -103,29 +75,38 @@ function Onimm(id, data_uri) {
 
 			onimm.settings.data = json;
 
-			onimm.board = onimm.container.append("svg:g")
-				.attr("class", "board");
-				
-			onimm.jobs = onimm.board.selectAll("g")
+			onimm.jobs = onimm.container.selectAll("g")
 				.data(onimm.settings.data);
 
-			onimm.jobsEnter = onimm.jobs.enter().append("svg:g")
-				.attr("class", "jobs")
-				.attr("class", "draggable");
+			onimm.jobs_enter = onimm.jobs.enter().append("svg:g")
+				.classed("jobs", function(d){return d;})
+				.classed("draggable", function(d) {return d;});
 
-			onimm.settings.totalNodes = onimm.jobsEnter.size();
+			onimm.settings.totalNodes = onimm.jobs_enter.size();
 				
 			onimm.jobs.append("svg:circle")
 					.attr("class", "circle")
-					.attr("r", 20)
+					.attr("r", onimm.settings.radius)
 					.attr("cx", function(d,i) {return d.x = onimm.x_coordinates(i);})
 					.attr("cy", function(d,i) {return d.y = onimm.y_coordinates(i);});
-			onimm.jobs.append("svg:text")
+
+			onimm.jobs_text = onimm.jobs.append("svg:text")
 					.attr("class", "data-text")
 					.attr("x", function(d,i) {return d.x = onimm.x_coordinates(i);})
 					.attr("y", function(d,i) {return d.y = onimm.y_coordinates(i);})
-					.text(function(d,i) {return d.name;});
-			
+					.attr("dx", "0")
+					.attr("dy", function(d,i) {return (1.5*onimm.settings.radius);})
+					.text(function(d,i) {return d.name})
+					.call(onimm.wrap, 10*onimm.settings.radius);
+
+			onimm.jobs.append("foreignObject")
+				.attr("width", 2*onimm.settings.radius)
+				.attr("height", 2*onimm.settings.radius)
+				.attr("x", function(d,i) {return d.x = onimm.x_coordinates(i) - onimm.settings.radius;})
+				.attr("y", function(d,i) {return d.y = onimm.y_coordinates(i) - onimm.settings.radius;})
+					.append("xhtml:body").attr("class", "foreignObject")
+						.html("<img class='bubble' src='./img/bubble.png'>");
+
 			onimm.jobs.call(onimm.settings.drag);
 			onimm.svg.call(onimm.settings.zoom);
 
@@ -137,14 +118,16 @@ function Onimm(id, data_uri) {
 	};
 
 	onimm.zoomed = function(d) {
-		console.log("X : " + d3.event.translate[0] + "  Y : " + d3.event.translate[1]);
-		onimm.svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+		
+		var new_x = d3.event.translate[0] + 0.5*onimm.settings.width;
+		var new_y = d3.event.translate[1] + 0.5*onimm.settings.height;
+		onimm.container.attr("transform", "translate(" + new_x + "," + new_y + ")");
 	};
 
 	onimm.zoomend = function(d) {
-
 	};
 
+	// ---- Drag'n'Drop elements ---
 	onimm.dragstarted = function(d) {
 		d3.event.sourceEvent.stopPropagation();
 		d3.select(this).classed("dragging", true);
@@ -153,8 +136,10 @@ function Onimm(id, data_uri) {
 	// Admitted the dragged element is a svg group g with internal circle and text
 	onimm.dragged = function(d) {
 		d3.select(this).select('circle').attr("cx", d3.event.x ).attr("cy", d3.event.y);
-		d3.select(this).attr("transfom", "translate("+ d3.event.x +","+ d3.event.y +")");
+		//d3.select(this).attr("transform", "translate("+ d3.event.x +","+ d3.event.y +")");
 		d3.select(this).select('text').attr("x", d3.event.x).attr("y", d3.event.y);
+		d3.select(this).select('foreignObject')
+			.attr("x", d3.event.x - onimm.settings.radius).attr("y", d3.event.y - onimm.settings.radius);
 		d.x = d3.event.x;
 		d.y = d3.event.y;
 	};
@@ -171,12 +156,47 @@ function Onimm(id, data_uri) {
 	onimm.x_coordinates = function(i) {
 		var x_coordinates = 0.4*(onimm.settings.height*Math.cos(i*(2*Math.PI)/onimm.settings.totalNodes));
 		return x_coordinates;
-	}
-
+	};
 	onimm.y_coordinates = function(i) {
 		var y_coordinates = 0.4*(onimm.settings.height*Math.sin(i*(2*Math.PI)/onimm.settings.totalNodes));
 		return y_coordinates;
-	}
+	};
+
+	/**
+	 * SVG text doesn't provide line break, so we have to use tspan
+	 * This function compute the breaking based on a width given, 
+	 * 3 radius
+	 */
+	onimm.wrap = function(text, width) {
+	  text.each(function() {
+	    var text = d3.select(this),
+	        words = text.text().split(/\s+/).reverse(),
+	        word,
+	        line = [],
+	        lineNumber = 0,
+	        lineHeight = 0.7,
+	        y = text.attr("y"),
+	        dy = parseFloat(text.attr("dy")),
+	        dx = parseFloat(text.attr("dx")),
+	        tspan = text.text(null).append("tspan").attr("x", text.x).attr("y", text.y).attr("dy", (dy) + "");
+	    while (word = words.pop()) {
+	      line.push(word);
+	      tspan.text(line.join(" "));
+	      if (tspan.node().getComputedTextLength() > width) {
+	        line.pop();
+	        tspan.text(line.join(" "));
+	        line = [word];
+	        tspan = text.append("tspan")
+	        .attr("x", text.x)
+	        .attr("y", text.y)
+	        .attr("dy", ++lineNumber * lineHeight + (1.2*dy) + "")
+	        .attr("dx", ++lineNumber * 0.02 * width + (3*dx) + "")
+	        .text(word);
+	      }
+	    }
+	  });
+	};
+
 
 	// Let it go, let it go !
 	onimm.init();
@@ -185,7 +205,7 @@ function Onimm(id, data_uri) {
 };
 
 // Let it go !
-onimm = Onimm("#onimm_", "http://www.jeremy-ta.fr/work/onisep/d3/data/test2.json");
+onimm = Onimm("onimm_", "./data/test2.json");
 
 // DEBUG
 //console.dir(onimm);
