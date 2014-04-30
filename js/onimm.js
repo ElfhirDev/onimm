ea96fd8ecfe36011accc3ef91adc7b71d0ce6202
Elfhir <ta.thanhliem@gmail.com>
2014-04-30 10:49:18 +0200

    Fix bug on chrome : padding on foreignObject

--- js/onimm.js
+++ js/onimm.js
@@ -2,7 +2,7 @@
  * 2014 © Onisep - tous droits réservés
  * 
  * Created by <jta@onisep.fr> 2014-04-14
- * Last update on 2014-04-14 by <jta@onisep.fr>
+ * Last update on 2014-04-29 by <jta@onisep.fr>
  *
  * Script aiming to render the mind map for a job
  *
@@ -25,12 +25,11 @@ function Onimm(id, data_uri) {
 		data_uri : data_uri,
 		x_coordinates : [],
 		y_coordinates : [],
-		margin : {top: -5, right: -5, bottom: -5, left: -5},
 		width : 600,
+		half_width : 300,
 		height : 400,
+		half_height : 200,
 		radius:20,
-		background_color : "#eee",
-		stroke_color : "#111",
 		hierarchie_color: "#FC8C2E",
 		collaboration_color: "#C9D800",
 		specialisation_color: "#0D7B92",
@@ -45,20 +44,9 @@ function Onimm(id, data_uri) {
 	 */
 	onimm.init = function() {
 
-		// ---- When panning the background -----
-		onimm.vars.zoom = d3.behavior.zoom()
-			.scaleExtent([1, 1])
-			.on("zoomstart", onimm.zoomstart)
-			.on("zoom", onimm.zoomed)
-			.on("zoomend", onimm.zoomend);
+		onimm.init_behavior();
 
-		// -------------- When dragging -----
-		onimm.vars.drag = d3.behavior.drag()
-			.on("dragstart", onimm.dragstarted)
-			.on("drag", onimm.dragged)
-			.on("dragend", onimm.dragended);
-
-		// ---------- Create SVG element -----
+		// Create SVG element container
 		onimm.svg = d3.select(onimm.vars.id).append("svg:svg")
 			.attr("width", onimm.vars.width)
 			.attr("height", onimm.vars.height)
@@ -66,17 +54,15 @@ function Onimm(id, data_uri) {
 			.style("border", "1px solid black")
 			.attr("id", id + "svg_");
 
-		// -- Create container of Bond(s), James Bond ---
+		// Create sub-container of Bond(s), James Bond
 		onimm.bond_container = onimm.svg.append("g")
-			.attr("transform", "translate(" + 0.5*onimm.vars.width + "," + 0.5*onimm.vars.height + ")")
+			.attr("transform", "translate(" + onimm.vars.half_width + "," + onimm.vars.half_height + ")")
 			.attr("class", "g_bond_container_");
 
-		// -- Create container of elements -----
+		// Create sub-container of other elements
 		onimm.container = onimm.svg.append("g")
-			.attr("transform", "translate(" + 0.5*onimm.vars.width + "," + 0.5*onimm.vars.height + ")")
+			.attr("transform", "translate(" + onimm.vars.half_width + "," + onimm.vars.half_height + ")")
 			.attr("class", "g_container_");
-
-
 
 		// Load our resources
 		d3.json(data_uri, function(error, json) {
@@ -89,11 +75,11 @@ function Onimm(id, data_uri) {
 			onimm.jobs = onimm.container.selectAll("g")
 				.data(onimm.vars.data);
 
-			onimm.jobs_enter = onimm.jobs.enter().append("svg:g")
+			onimm.jobs = onimm.jobs.enter().append("svg:g")
 				.classed("jobs", function(d){return d;})
 				.classed("draggable", function(d) {return d;});
 
-			onimm.vars.totalNodes = onimm.jobs_enter.size();
+			onimm.vars.totalNodes = onimm.jobs.size();
 
 			onimm.jobs.append("svg:circle")
 					.attr("class", "circle")
@@ -107,46 +93,7 @@ function Onimm(id, data_uri) {
 						return d.y = onimm.init_y_coordinates(d,i);
 					});
 
-			// ---- Path joining Nodes Jobs ----
-			onimm.bonds = [];
-			for(var a = 0, l = onimm.vars.totalNodes; a<l; a++) {
-
-				if(onimm.vars.data[a].isActive === false){
-
-					onimm.bonds[a] = onimm.bond_container.append("path")
-						.attr("class", function(d,i) {return "bond_"})
-						.attr("id", function(d,i) {return "bond_"+a})
-						.attr("fill", "none").attr("stroke-width", "6").attr("stroke", "none")
-						.attr("d", "M 0,0 0,0 0,0 "+onimm.vars.x_coordinates[a]+","+onimm.vars.y_coordinates[a]+"");
-				}
-				else {		
-					onimm.bonds[a] = "isActive";	// current node active doesn't need bond		
-					onimm.hierarchie = onimm.vars.data[a].hierarchie;	
-					onimm.specialisation = onimm.vars.data[a].specialisation;
-					onimm.collaboration = onimm.vars.data[a].collaboration;
-				}
-
-			}
-
-			for (var a = 0, l = onimm.vars.totalNodes; a<l; a++) {
-				// node active doesn't have path svg, so no attr()
-				
-				if (onimm.bonds[a] != "isActive") {
-					
-					// Use jQuery inArray, in future use built-in indexOf (<I.E9)
-					if (-1 != $.inArray(a, onimm.hierarchie)) {
-						onimm.bonds[a].attr("stroke", "#FC8C2E");
-					}
-
-					if (-1 != $.inArray(a, onimm.specialisation)) {
-						onimm.bonds[a].attr("stroke", "#0D7B92");
-					}
-
-					if (-1 != $.inArray(a, onimm.collaboration)) {
-						onimm.bonds[a].attr("stroke", "#C9D800");
-					}
-				}
-			}
+			onimm.init_bonds();
 
 			onimm.jobs_text = onimm.jobs.append("svg:text")
 					.attr("class", "data-text")
@@ -163,12 +110,13 @@ function Onimm(id, data_uri) {
 					.text(function(d,i) {return d.name})
 					.call(onimm.wrap, 10*onimm.vars.radius);
 
-			onimm.jobs.append("foreignObject")
+			onimm.bubble = onimm.jobs.append("svg:foreignObject")
+				.attr("class", "bubble-foreignObject")
 				.attr("width", 2*onimm.vars.radius)
 				.attr("height", 2*onimm.vars.radius)
 				.attr("x", function(d,i) {return d.x = onimm.init_x_coordinates(d,i) - onimm.vars.radius;})
 				.attr("y", function(d,i) {return d.y = onimm.init_y_coordinates(d,i) - onimm.vars.radius;})
-					.append("xhtml:body").attr("class", "foreignObject")
+					.append("xhtml:body").attr("class", "bubble-body")
 						.html("<img class='bubble' src='./img/bubble.png'>");
 
 
@@ -184,7 +132,6 @@ function Onimm(id, data_uri) {
 				onimm.modale = onimm.svg.append("svg:svg");
 
 				onimm.modale
-					.transition().duration(3000).delay(500)
 					.attr("width", onimm.vars.width - 20)
 					.attr("height", onimm.vars.height - 20)
 					.attr("align", "center")
@@ -192,24 +139,28 @@ function Onimm(id, data_uri) {
 					.attr("id", id + "modale_");
 
 				// -- Create container of elements -----
-				onimm.modale_rect = onimm.modale.append("svg:rect");
-
-				onimm.modale_rect
-					.transition().duration(3000).delay(500)
+				onimm.modale_rect = onimm.modale.append("svg:rect")
 					.attr("transform", "translate(" + 20 + "," + 20 + ")")
-					.attr("width", (onimm.vars.width-40))
-					.attr("height", (onimm.vars.height-40))
+					.attr("width", onimm.vars.width)
+					.attr("height", onimm.vars.height)
 					.style("fill", "rgba(255,255,255,0.9)");
 
-				onimm.modale_leave = onimm.modale.append("foreignObject");
+				// -- elastic animation ----- 
+				onimm.modale_rect
+					.transition().duration(1500)
+					.attr("width", (onimm.vars.width-40))
+					.attr("height", (onimm.vars.height-40));
+					
+
+				onimm.modale_leave = onimm.modale.append("foreignObject").attr("class","modale");
 
 				onimm.modale_leave
 					.attr("width", 30)
 					.attr("height", 30)
 					.attr("x", onimm.vars.width - 50)
 					.attr("y", (onimm.vars.height - 380))
-						.append("xhtml:body").attr("class", "foreignObject")
-							.html("<img class='close-icon' src='./img/close-icon.png'>");
+						.append("xhtml:body").attr("class", "modale-body")
+							.html("<img class='modale-close-icon' src='./img/close-icon.png'>");
 
 				onimm.modale_title = onimm.modale.append("svg:text");
 
@@ -219,6 +170,7 @@ function Onimm(id, data_uri) {
 					.attr("x", 0.4*onimm.vars.width)
 					.attr("y", 30)
 					.text(d.name);
+
 
 				// If we click on the close button
 				onimm.modale_leave.on("click", function(d) {
@@ -230,13 +182,30 @@ function Onimm(id, data_uri) {
 		}); // End d3.json(uri,function)
 	};
 
+	/* ------ methods ------- */
+
+	onimm.init_behavior = function() {
+		// When moving the background
+		onimm.vars.zoom = d3.behavior.zoom()
+			.scaleExtent([1, 1])
+			.on("zoomstart", onimm.zoomstart)
+			.on("zoom", onimm.zoomed)
+			.on("zoomend", onimm.zoomend);
+
+		// When dragging a node
+		onimm.vars.drag = d3.behavior.drag()
+			.on("dragstart", onimm.dragstarted)
+			.on("drag", onimm.dragged)
+			.on("dragend", onimm.dragended);
+	};
+
 	onimm.zoomstart = function(d) {
 		d3.event.sourceEvent.stopPropagation();
 	};
 
 	onimm.zoomed = function(d) {
-		var new_x = d3.event.translate[0] + 0.5*onimm.vars.width;
-		var new_y = d3.event.translate[1] + 0.5*onimm.vars.height;
+		var new_x = d3.event.translate[0] + onimm.vars.half_width;
+		var new_y = d3.event.translate[1] + onimm.vars.half_height;
 		onimm.container.attr("transform", "translate(" + new_x + "," + new_y + ")");
 		onimm.bond_container.attr("transform", "translate(" + new_x + "," + new_y + ")");
 	};
@@ -254,9 +223,10 @@ function Onimm(id, data_uri) {
 	onimm.dragged = function(d) {
 		d3.select(this).select('circle').attr("cx", d3.event.x ).attr("cy", d3.event.y);
 		//d3.select(this).attr("transform", "translate("+ d3.event.x +","+ d3.event.y +")");
-		d3.select(this).select('text').attr("x", d3.event.x).attr("y", d3.event.y);
-		d3.select(this).select('foreignObject')
-			.attr("x", d3.event.x - onimm.vars.radius).attr("y", d3.event.y - onimm.vars.radius);
+		d3.select(this).select('text').attr("x", d3.event.x).attr("y", d3.event.y)
+			.on("mousedown", function() { d3.event.stopPropagation(); });
+		d3.select(this).select('.bubble-foreignObject')
+			.attr("x", d3.event.x - onimm.vars.radius).attr("y", d3.event.y - onimm.vars.radius)
 
 		d.x = d3.event.x;
 		d.y = d3.event.y;
@@ -305,6 +275,44 @@ function Onimm(id, data_uri) {
 			return y_coordinates;
 		}
 	};
+
+	/**
+	 * Create bonds
+	 */
+	onimm.init_bonds = function() {
+		onimm.bonds = [];
+		for(var a = 0, l = onimm.vars.totalNodes; a<l; a++) {
+			if(onimm.vars.data[a].isActive === false){
+
+				onimm.bonds[a] = onimm.bond_container.append("path")
+					.attr("class", function(d,i) {return "bond_"})
+					.attr("id", function(d,i) {return "bond_"+a})
+					.attr("fill", "none").attr("stroke-width", "6").attr("stroke", "none")
+					.attr("d", "M 0,0 0,0 0,0 "+onimm.vars.x_coordinates[a]+","+onimm.vars.y_coordinates[a]+"");
+			}
+			else {		
+				onimm.bonds[a] = "isActive";	// current node active doesn't need bond		
+				onimm.hierarchie = onimm.vars.data[a].hierarchie;	
+				onimm.specialisation = onimm.vars.data[a].specialisation;
+				onimm.collaboration = onimm.vars.data[a].collaboration;
+			}
+		}
+		for (var a = 0, l = onimm.vars.totalNodes; a<l; a++) {
+			// node active doesn't have path svg, so no attr()
+			if (onimm.bonds[a] != "isActive") {	
+				// Use jQuery inArray, in future use built-in indexOf (<I.E9)
+				if (-1 != $.inArray(a, onimm.hierarchie)) {
+					onimm.bonds[a].attr("stroke", "#FC8C2E");
+				}
+				if (-1 != $.inArray(a, onimm.specialisation)) {
+					onimm.bonds[a].attr("stroke", "#0D7B92");
+				}
+				if (-1 != $.inArray(a, onimm.collaboration)) {
+					onimm.bonds[a].attr("stroke", "#C9D800");
+				}
+			}
+		}
+	}
 
 	/**
 	 * SVG text doesn't provide line break, so we have to use tspan
